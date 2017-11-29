39f39625b33799b9d373a29ecc71c40f2473ec32
limuxiao <li19890814@163.com>
2017-11-29 20:46:15 +0800

    add class

---
+++ week01/TestClass.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from week01.Cat import Cat
+
+
+def testClass01():
+    c = Cat()
+    c.test()
+    c.sleep()
+    pass
+
+
+def main():
+    testClass01()
+    pass
+
+
+if __name__ == '__main__':
+    main()
