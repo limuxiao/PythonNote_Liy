ec79216fa5f089c64e6f61055f6c12c1b2615083
limuxiao <li19890814@163.com>
2017-12-01 17:04:29 +0800

    add test code

---
+++ week01/TestModule/TestModule.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+import os
+import pygame
+
+def test01():
+    print(os.__file__)
+    print(pygame.__file__)
+    print(os.__all__)
+    pass
+
+
+
+def main():
+    test01()
+    pass
+
+
+if __name__ == '__main__':
+    main()
