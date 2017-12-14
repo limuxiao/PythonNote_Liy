39cbe44cfd1355c4cc76b7a394d3940eee344722
limuxiao <li19890814@163.com>
2017-12-14 09:12:56 +0800

    update productor and consumer

--- week03/测试线程/简单的生产者与消费者模式.py
+++ week03/测试线程/简单的生产者与消费者模式.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
+import threading
 from threading import Thread
 from queue import Queue
+import time
 
 
 class Factory(Thread):
@@ -8,7 +10,8 @@ class Factory(Thread):
         工厂类
     """
     def __init__(self, queue):
-        self.__queue = queue
+        super().__init__()
+        self.queue = queue
     pass
 
 
@@ -18,7 +21,14 @@ class Producer(Factory):
     """
 
     def run(self):
-        super().run()
+        while True:
+            if self.queue.qsize() < 1000:
+                print('----需要生产----生产线程id:%s----生产产品id:%d----' % (threading.current_thread().name, self.queue.qsize()+1))
+                self.queue.put(self.queue.qsize() + 1)
+                time.sleep(0.1)
+            else:
+                print('----不需要生产----生产线程id:%s----' % threading.current_thread().name)
+                time.sleep(0.5)
 
 
 class Consumer(Factory):
@@ -27,11 +37,23 @@ class Consumer(Factory):
     """
 
     def run(self):
-        super().run()
+        while True:
+            if self.queue.qsize() > 100:
+                print('----可以消费----消费线程id:%s----消费产品id:%d----' % (threading.current_thread().name, self.queue.get()))
+                time.sleep(0.2)
+            else:
+                print('----不可以消费----消费线程id:%s----' % threading.current_thread().name)
+                time.sleep(0.5)
 
 
 def f1():
-    pass
+    queue = Queue()
+    for i in range(5):
+        p = Producer(queue)
+        p.start()
+    for i in range(3):
+        c = Consumer(queue)
+        c.start()
 
 
 def main():
