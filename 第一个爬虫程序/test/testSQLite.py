6c5e7ce294879902081696e67a0c4af7733cdf33
limuxiao <li19890814@163.com>
2018-04-28 09:04:44 +0800

    add webservice

--- 第一个爬虫程序/test/testSQLite.py
+++ 第一个爬虫程序/test/testSQLite.py
@@ -1,15 +1,48 @@
 # -*- coding:utf-8 -*-
 import sqlite3
+import pymysql
+
 
 
 def f1():
     conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE yzg_goods (id int, data char(50))
+    ''')
+    c.execute('''
+        INSERT INTO yzg_goods values(1, 'haha')
+    ''')
+
+    c.execute('''
+            INSERT INTO yzg_goods values(1, 'haha')
+    ''')
+
+    conn.commit()
+    conn.close()
     pass
 
 
+def f2():
+    help(sqlite3)
+    pass
+
+
+def f3():
+    # help(pymysql)
+    connection = pymysql.connect(host='127.0.0.1',
+                                 port=3306,
+                                 user='root',
+                                 password='123456',
+                                 db='lmx',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+
 def main():
-    f1()
+    # f1()
+    # f2()
+    f3()
     pass
 
 
